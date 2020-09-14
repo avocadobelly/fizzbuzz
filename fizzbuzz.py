7e928cdff27ac153cbc453c418fb4bfb2e1280f0
gconnellbass <georgia.connell-bass@altran.com>
2020-09-14 11:13:36 +0100

    created fizzbuzz file

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+#Write a program that prints the numbers from 1 to 100.
+#If a number is a multiple of three, print "Fizz" instead of the number.
+#If the number is a multiple of five print "Buzz" instead of the number.
+#For numbers which are multiples of both three and five print "FizzBuzz" instead of the number.
+
+
+i = 1
+
+while i <= 100
+    print i
+    i ++
+    
